c2de40e02fb0311e2b7712c5ebe2654e76e396d7
torbenküstermann <kuestermann.to-it21@it.dhbw-ravensburg.de>
2024-04-22 23:37:14 +0200

    Initial Blink LED

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -97,6 +97,8 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_TogglePin (GPIOA, GPIO_PIN_5);
+	  HAL_Delay(5000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
